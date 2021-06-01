ecc0e9c578dbf05f298a6339f9af89f37ed93959
danlyth <48352648+danlyth@users.noreply.github.com>
2021-06-01 16:25:45 +0100

    Adding some comments

--- loss_function.py
+++ loss_function.py
@@ -20,26 +20,27 @@ class Tacotron2Loss_VAE(nn.Module):
                 return min(upper, step/x0)
             else:
                 return 0
-        elif anneal_function == 'constant':
+        elif anneal_function == 'constant': # simple beta-VAE
             return 0.001
 
 
-    def forward(self, model_output, targets, step):
+    def forward(self, model_output, targets, step): # NOTE model ouput==y_pred in train.py
         mel_target, gate_target = targets[0], targets[1]
         mel_target.requires_grad = False
         gate_target.requires_grad = False
         gate_target = gate_target.view(-1, 1)
 
-        mel_out, mel_out_postnet, gate_out, _, mu, logvar, _, _ = model_output
+        mel_out, mel_out_postnet, gate_out, _, mu, logvar, _, _ = model_output # NOTE mu and logvar are both nan, mel_out, mel_out_postnet are all 0 and gate_out is all 1000
         gate_out = gate_out.view(-1, 1)
         mel_loss = nn.MSELoss()(mel_out, mel_target) + \
             nn.MSELoss()(mel_out_postnet, mel_target)
         gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
-
+        # print(f'Log var: {logvar}, \nMu: {mu}, \nMu power: {mu.pow(2)}, \nLog var exp: {logvar.exp()}')
+        # print(f'mel_out: {mel_out}, \nmel_out_posnet: {mel_out_postnet}, \ngate_out: {gate_out}')
         kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         kl_weight = self.kl_anneal_function(self.anneal_function, self.lag, step, self.k, self.x0, self.upper)
         
         recon_loss = mel_loss + gate_loss
         total_loss = recon_loss + kl_weight*kl_loss
-
+        # print(f'Total loss:{total_loss}, Recon loss: {recon_loss}, KL loss: {kl_loss}, KL weight: {kl_weight}')
         return total_loss, recon_loss, kl_loss, kl_weight
